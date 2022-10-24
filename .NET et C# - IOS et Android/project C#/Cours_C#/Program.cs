1e90863a3d42edd7a83595e6b874975cd4a7af1b
Steelataure <alexandre0312@orange.fr>
2022-10-24 14:32:44 +0200

    docnet

---
+++ .NET et C# - IOS et Android/project C#/Cours_C#/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Test;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[]tableau = {"A","B","C","D","E"};
+        foreach(string i in tableau){
+            System.Console.WriteLine($"Lettre {i}"); 
+        }
+        
+    }
+}
